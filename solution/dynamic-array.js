b5a28acc94d37b056fc77f3a47027d7e7742eda2
dcsiona@gmail.com <dcsiona@gmail.com>
2024-03-15 16:16:38 -0400

    another comit

--- solution/dynamic-array.js
+++ solution/dynamic-array.js
@@ -1,19 +1,45 @@
 class DynamicArray {
 
-  constructor(defaultSize=4) {
+  constructor(defaultSize = 4) {
 
     // Your code here
+    this.data = new Array(defaultSize);
+    this.length = 0;
+    this.capacity = defaultSize;
 
   }
 
   read(index) {
 
     // Your code here
+    if (index >= 0 && index < this.length) {
+      return this.data[index];
+    } else {
+      return undefined;
+    }
   }
 
   unshift(val) {
 
     // Your code here
+    if (this.length === this.capacity) {
+      this._resize(this.capacity * 2);
+    }
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = val;
+    this.length++;
+  }
+
+  _resize(newCapacity) {
+    const newData = newArray(newCapacity);
+    for (let i = 0; i < this.length; i++) {
+      newData[i] = this.data[i];
+    }
+    this.data = newData;
+    this.capacity = newCapacity;
+    this.length = 0;
   }
 
 }
